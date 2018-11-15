faa4a29098c349db0e0cba224cefd8935cfcb5ff
Utkarsha Jitendra Ganla <uganla@uci.edu>
2018-11-15 07:30:31 -0800

    mockwebserver

--- okhttp/src/main/java/module-info.java
+++ okhttp/src/main/java/module-info.java
@@ -2,7 +2,9 @@ module okhttp {
     exports okhttp3.internal;
     exports okhttp3.internal.platform;
     exports okhttp3;
+    exports okhttp3.internal.ws;
     exports okhttp3.internal.http;
+    exports okhttp3.internal.http2;
     requires okio;
     requires java.logging;
     requires jsr305;
